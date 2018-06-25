bb8cf1e7bb75481d6fc94d2ef06c0573e5b33d0d
Colin Tindle <ampersand@underscore.space>
2018-06-25 18:53:04 +0100

    added comments for wordCount, since it's a long statement

--- app/src/models/wordCounter.js
+++ app/src/models/wordCounter.js
@@ -1,3 +1,9 @@
-const wordCount = words => words.length===0?0:words.trim().replace(/\s+/g, ' ').split(' ').length
+const wordCount = words => {
+  return words.length===0?0:  // return 0 if empty string
+    words.trim()              // remove spaces at start and end
+      .replace(/\s+/g, ' ')   // remove duplicate spaces
+      .split(' ')             // split by remaining spaces
+      .length                 // return count
+}
 
 module.exports = wordCount
